c2a9fed14458229e24f785c65651e56a73ce6d86
Jatin Tekam <tekamjatin@gmail.com>
2024-10-13 18:36:13 +0530

    add output statment for name1

--- program1.java
+++ program1.java
@@ -2,8 +2,9 @@ class program1{
 
 public static void main(String args[]){
 int age=20;
-String name="Jatin Tekam";
+String name1="Jatin Tekam";
 System.out.println("Hello World");
+System.out.println(name1);
 
 
 }
